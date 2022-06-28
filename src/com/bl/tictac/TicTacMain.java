d86638ea1058a2ddec418de1daf921fcad1f4ea8
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-06-28 18:09:04 +0530

    added code to show board

--- src/com/bl/tictac/TicTacMain.java
+++ src/com/bl/tictac/TicTacMain.java
@@ -5,8 +5,8 @@ public class TicTacMain {
         System.out.println("welcome to tic tac toe game");
         TicTacToe game = new TicTacToe();
         game.Board();
-        game.showBoard();
         game.selectOption();
+        game.showBoard();
     }
 }
 
--- src/com/bl/tictac/TicTacToe.java
+++ src/com/bl/tictac/TicTacToe.java
@@ -3,30 +3,30 @@ package com.bl.tictac;
 import java.util.Scanner;
 
 public class TicTacToe {
-    Scanner input = new Scanner(System.in);
-    char userLetter, computerLetter;
-    char[] index = new char[10];
 
-    public void Board() {
-        for (int i = 1; i < index.length; i++) {
-            index[i] = ' ';
+        Scanner input = new Scanner(System.in);
+        char userLetter, computerLetter;
+        char[] index = new char[10];
+
+        public void Board() {
+            for (int i = 1; i < index.length; i++) {
+                index[i] = ' ';
+            }
         }
+
+        public void showBoard() {
+            System.out.println(index[1] + "   " + " | " + index[2] + "  " + " | " + index[3] + " " + "\n" +
+                    index[4] + "   " + " | " + index[5] + "  " + " | " + index[6] + " " + " " + "\n" +
+                    index[7] + "   " + " | " + index[8] + "  " + " | " + index[9] + " ");
+        }
+
+        public void selectOption() {
+            System.out.println("Select your letter : X?O");
+            userLetter = input.next().toUpperCase().charAt(0);
+            computerLetter = (userLetter == 'X' ? 'O' : 'X');
+            System.out.println("User Letter: " + userLetter + ", " + "Computer Letter: " + computerLetter);
+        }
+
+
     }
-
-    public void showBoard() {
-        System.out.println(index[1] + "   " + " | " + index[2] + "  " + " | " + index[3] + " " + "\n" +
-                index[4] + "   " + " | " + index[5] + "  " + " | " + index[6] + " " + " " + "\n" +
-                index[7] + "   " + " | " + index[8] + "  " + " | " + index[9] + " ");
-    }
-
-    public void selectOption() {
-        System.out.println("Select  letter : X?O");
-        userLetter = input.next().toUpperCase().charAt(0);
-        computerLetter = (userLetter == 'X' ? 'O' : 'X');
-        System.out.println("User Letter: " + userLetter + ", " + "Computer Letter: " + computerLetter);
-    }
-
-
-}
-
 
